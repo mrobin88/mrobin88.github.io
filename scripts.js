54fe807eb9fc43d1bb88480d29e8cc98d84314f9
Matthew Robin <RatthewRobin@gmail.com>
2017-09-04 12:50:56 -0400

    not more videos

--- scripts.js
+++ scripts.js
@@ -1,17 +1,27 @@
-<script src="jquery.fitvids.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	$(document).ready(function(){
+	var placement = document.getElementById("place");
+	var logo = document.getElementById("logo")
 
-$(document).ready(function(){
-	// Target your .container, .wrapper, .post, etc.
+window.onload = function(){
+function logoGen(){
+alert("you are cool")
+/*
+for (var line=1; line<4; line++) {
+ for(var i=1;i<4;i++) {
+   document.getElementById('place').innerText += (Math.floor((Math.random()*2)%2)) ? "╱" : "╲";
+ 
+ }
+ document.writeln("<br>");
+}
+*/
+
+}
+// Target your .container, .wrapper, .post, etc.
 	$("#thing-with-videos").fitVids();
-});
 
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-68568528-1', 'auto');
-  ga('send', 'pageview');
 
+}
+
+}
