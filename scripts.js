ad185ecce7f945c6363aac6934a6426a6d9179cc
mrobin88 <ratthewrobin@gmail.com>
2023-05-09 08:56:17 -0700

    updating the content of the site and making sure I have the latest npm installed as well as testing stuff for running locally. plans to build out some animations and eventally move to a react application

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
-function Copy() {
 
-    copyText = document.getElementById("email");
-    copyText.select();
-    document.execCommand("copy");
-    document.getElementById("MTGAOK").style.visibility = "visible";
-}
+
+function Copy() {
+    // Copy email to clipboard
+    const email = 'ratthewrobin@gmail.com';
+    navigator.clipboard.writeText(email);
+  
+    // Display notification
+    const notification = document.createElement('div');
+    notification.textContent = 'Email copied!';
+    notification.classList.add('notification');
+  
+    document.body.appendChild(notification);
+  
+    // Remove notification after 2 seconds
+    setTimeout(() => {
+      notification.remove();
+    }, 2000);
+  }
+  
